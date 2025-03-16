8556c9447c08e06adac37ba44915929fc079c63f
Vladimir Revyagin <skorabeyf@gmail.com>
2025-03-16 20:02:16 +0300

    feat: к-т навбар + роуты

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -1,22 +1,36 @@
-import {
-  RouterProvider,
-  createBrowserRouter,
-} from "react-router-dom";
-import { AppRoutes } from "@/shared/ui/const/router";
-import { MainPage } from "@/pages/MainPage";
-import { LoginPage } from "@/pages/LoginPage";
+import { Outlet, RouterProvider, createBrowserRouter } from 'react-router-dom'
+import { AppRoutes } from '@/shared/ui/const/router'
+import { MainPage } from '@/pages/MainPage'
+import { LoginPage } from '@/pages/LoginPage'
+import { NavBar } from '@/widgets/NavBar/ui'
+
+const HeaderLayout = () => {
+  return (
+    <>
+      <header>
+        <NavBar />
+      </header>
+      <Outlet />
+    </>
+  )
+}
 
 const router = createBrowserRouter([
   {
-    path: AppRoutes.MAIN,
-    element: <MainPage />,
+    element: <HeaderLayout />,
+    children: [
+      {
+        path: AppRoutes.MAIN,
+        element: <MainPage />,
+      },
+      {
+        path: AppRoutes.LOGIN,
+        element: <LoginPage />,
+      },
+    ],
   },
-  {
-    path:AppRoutes.LOGIN,
-    element: <LoginPage/>
-  },
-]);
+])
 
 export function AppRouter() {
-  return <RouterProvider router={router} />;
+  return <RouterProvider router={router} />
 }
